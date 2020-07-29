b0de217d51d3b1185b51c3433dc48ff1059fd2b4
adhiiisetiawan <adhisetiawan518@gmail.com>
2020-07-29 17:25:11 +0700

    intent to NotificationService

--- app/src/main/java/com/example/customnotification/MainActivity.java
+++ app/src/main/java/com/example/customnotification/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.customnotification;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonNotif = findViewById(R.id.btn_show_notif);
+        buttonNotif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this,NotificationService.class));
+            }
+        });
     }
 }
